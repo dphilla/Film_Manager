5c4d8100470d82e6d5b67a36b0366014e54c053e
Daniel Phillips <danieledward.phillips@gmail.com>
2017-06-28 17:34:19 -0600

    adds films table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628211847) do
+ActiveRecord::Schema.define(version: 20170628231215) do
+
+  create_table "films", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "year"
+    t.integer  "box_office_sales"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "genres", force: :cascade do |t|
     t.string   "name"
